514260ca052b9d864c9121474a3fe0591aa7abe4
Adrian2004-ram <adrianespac@gmail.com>
2025-11-18 14:34:04 +0100

    creamos UserController

--- src/test/java/com/iesvdm/crud_jdbc_project/CrudJdbcProjectApplicationTests.java
+++ src/test/java/com/iesvdm/crud_jdbc_project/CrudJdbcProjectApplicationTests.java
@@ -4,6 +4,7 @@ import com.iesvdm.crud_jdbc_project.dao.UserDAO;
 import com.iesvdm.crud_jdbc_project.model.User;
 import com.iesvdm.crud_jdbc_project.util.HashUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,23 +24,29 @@ class CrudJdbcProjectApplicationTests {
     }
 
     @Test
-    void createuserTest() {
+    void createUserAdminTest() {
 
         String username = "jose";
         String password = "1234";
 
+        String passawordHashed = null;
+
         try {
 
-            String passawordHashed =  hashUtil.hashPassword(password);
+            passawordHashed =  hashUtil.hashPassword(password);
+
             log.info("Password hashed: " + passawordHashed);
+
 
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-        User user = User.builder().username(username).password(password).build();
+        User user = User.builder().username(username).password(passawordHashed).build();
 
         userDAO.create(user);
+
+        Assertions.assertTrue(user.getId()>0);
 
     }
 
